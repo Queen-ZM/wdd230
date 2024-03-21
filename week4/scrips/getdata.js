53b780eae2d1bc2d4dc5176e93b855224df20c45
zil22002@byui.edu <zilimbolanoluthando1991@gmail.com>
2024-03-21 19:26:30 +0200

    folder creations

---
+++ week4/scrips/getdata.js
@@ -0,0 +1,34 @@
+let d = new Date();
+
+let newUpdate = document.getElementById('footer-sect');
+newUpdate.querySelector('#currentYear').innerHTML = d.getFullYear();
+newUpdate.querySelector('#currentDateAndTime').innerHTML = document.lastModified;
+
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+  navigation.classList.toggle('open');
+  hamButton.classList.toggle('open');
+});
+
+let visitCount = localStorage.getItem('visitCount');
+
+if (visitCount === null) {
+    
+    visitCount = 0;
+} else {
+    
+    visitCount = parseInt(visitCount);
+}
+
+const visitCountElement = document.getElementById('visit-count');
+visitCountElement.textContent = visitCount;
+
+visitCount++;
+visitCountElement.textContent = visitCount;
+
+
+localStorage.setItem('visitCount', visitCount.toString());
